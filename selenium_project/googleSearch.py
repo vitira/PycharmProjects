01c3e09dc78391b73472bd5ca9c68bb47776699c
vitira <69127561+vitira@users.noreply.github.com>
2021-04-11 23:46:03 -0400

    Update googleSearch.py
    delete close browser

--- selenium_project/googleSearch.py
+++ selenium_project/googleSearch.py
@@ -12,5 +12,3 @@ search_text_box = driver.find_element_by_name("q")
 search_text_box.clear()
 search_text_box.send_keys("python selenium integration")
 search_text_box.send_keys(Keys.RETURN)
-
-driver.close()
